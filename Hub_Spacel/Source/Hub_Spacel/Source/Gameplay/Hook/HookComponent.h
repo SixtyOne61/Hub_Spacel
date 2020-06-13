c5686f0f2c254a2065ae164f158635c2f66b7a24
SixtyOne61 <olivier.lavalley@gmail.com>
2020-06-13 12:42:22 +0200

    add hookComponent

---
+++ Hub_Spacel/Source/Hub_Spacel/Source/Gameplay/Hook/HookComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "HookComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HUB_SPACEL_API UHookComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
