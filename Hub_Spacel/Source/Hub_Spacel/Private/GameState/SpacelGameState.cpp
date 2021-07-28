eda273d988440714ac27ea16d207d54841aace7a
Icarhum <olivier.lavalley@gmail.com>
2021-07-28 11:56:28 +0200

    update visuel pirate / init rand with millisecond

--- Hub_Spacel/Source/Hub_Spacel/Private/GameState/SpacelGameState.cpp
+++ Hub_Spacel/Source/Hub_Spacel/Private/GameState/SpacelGameState.cpp
@@ -138,7 +138,7 @@ void ASpacelGameState::RegisterTeam()
 void ASpacelGameState::BeginPlay()
 {
     Super::BeginPlay();
-    FMath::RandInit((int32)FDateTime::Now().ToUnixTimestamp());
+    FMath::RandInit((int32)FDateTime::Now().GetMillisecond());
 }
 
 void ASpacelGameState::RPCNetMulticastStartMission_Implementation(EMission _type)
