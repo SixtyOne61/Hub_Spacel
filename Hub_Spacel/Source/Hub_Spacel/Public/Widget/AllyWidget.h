2ed807fea0519ff41bc2bb36927a4013e6434283
Icarhum <olivier.lavalley@gmail.com>
2021-09-22 02:44:07 +0200

    update ally widget

--- Hub_Spacel/Source/Hub_Spacel/Public/Widget/AllyWidget.h
+++ Hub_Spacel/Source/Hub_Spacel/Public/Widget/AllyWidget.h
@@ -27,15 +27,16 @@ protected:
 	UFUNCTION()
 	void UpdateWatcher();
 
+protected:
+	UPROPERTY(Category = "DataAsset", EditAnywhere, BlueprintReadWrite)
+	class UTeamColorDataAsset* TeamColorDataAsset{ nullptr };
+
 private:
 	UPROPERTY()
 	class UTextBlock* NameTextBlock { nullptr };
 
 	UPROPERTY()
 	class UProgressBar* ProtectionProgressBar { nullptr };
-
-	UPROPERTY()
-	class UProgressBar* SupportProgressBar { nullptr };
 
 	UPROPERTY()
 	class UImage* DisconnectImage { nullptr };
